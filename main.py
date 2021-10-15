7f3c6987a0e02a1748c5685d3c29da2922cef873
veritas <rose.daemon.belmont@gmail.com>
2021-10-14 17:34:16 -0700

    adds kick, ban, and unban

--- main.py
+++ main.py
@@ -78,15 +78,26 @@ async def clear (ctx, amount=1):
     await ctx.channel.purge(limit=amount)
 
 #little kick command for fun
-@bot.command():
-async def kick(ctx, member : discord.member, *, reason=None):
+@bot.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
     await member.kick(reason=reason)
 
 # we fuck around and find out
-@bot.command():
-async def ban(ctx, member : discord.member, *, reason=None):
+@bot.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
     await member.ban(reason=reason)
 
+# we unfuck
+@bot.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
 
 
 bot.run(TOKEN)
